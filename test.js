f812d26fa50f561b184d7b8eb4f6acbda3e14fa1
VToribiot <vtoribiot@gmail.com>
2022-06-15 19:54:38 -0400

    2nd test

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-import { Range, range } from "./main"
+const { Range, range } = require('./main')
+
 
 test('Integer range contains', ()=>{
-    const range1 = [2, 3, 4]
-    expect(Range.contain(range, range1)).toBeTruthy()
+    const arr = [2, 3, 4]
+    expect(Range.contain(range, arr)).toBeTruthy()
+})
+
+test('Integer range not contains', () => {
+    const arr = [-1, 1, 6, 10]
+    expect(Range.contain(range, arr)).toBeTruthy()
 })
