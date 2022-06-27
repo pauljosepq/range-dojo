8f7389cb55e6e1163af46689419f07dd1bb8f9c2
Paul Paniagua <70442497+pauljosepq@users.noreply.github.com>
2022-06-27 00:06:34 +0000

    [IMP] Tests

--- test.js
+++ test.js
@@ -1,32 +1,40 @@
 const { Range } = require('./main')
-const range = [2, 3, 4, 5]
+const range = '[2, 6)'
 const r = new Range(range)
 
 
 test('Integer range contains', ()=>{
-    const arr = [2, 4]
+    const arr = '2, 4'
     expect(r.contains(arr)).toBeTruthy()
 })
 
 test('Integer range not contains', () => {
-    const arr = [-1, 1, 6, 10]
+    const arr = '-1, 1, 6, 10'
     expect(r.contains(arr)).toBeFalsy()
 })
 
-test('Range not contains [7, 9]', () => {
-    const arr = [7, 8, 9]
-    expect(r.contains(arr)).toBeFalsy()
+test('Range not contains [7, 10)', () => {
+    const range1 = '[2, 5)'
+    const r1 = new Range(range1)
+    const range2 = '[7, 10)'
+    const r2 = new Range(range2)
+    expect(r1.contains(r2.range)).toBeFalsy()
 })
 
-test('Range not contains [3, 9]', () => {
-    const arr = [3, 4, 5, 6 ,7 ,8, 9]
-    expect(r.contains(arr)).toBeFalsy()
+test('Range not contains [3, 10)', () => {
+    const range1 = '[2, 5)'
+    const r1 = new Range(range1)
+    const range2 = '[3, 10)'
+    const r2 = new Range(range2)
+    expect(r1.contains(r2.range)).toBeFalsy()
 })
 
-test('Range not contains [2, 9]', () => {
-    const r1 = new Range([3, 4])
-    const arr = [2, 3, 4, 5, 6, 7, 8, 9]
-    expect(r1.contains(arr)).toBeFalsy()
+test('Range not contains [2, 10)', () => {
+    const range1 = '[3, 5)'
+    const r1 = new Range(range1)
+    const range2 = '[2, 10)'
+    const r2 = new Range(range2)
+    expect(r1.contains(r2.range)).toBeFalsy()
 })
 
 test('Range contains [3, 5]', () => {
